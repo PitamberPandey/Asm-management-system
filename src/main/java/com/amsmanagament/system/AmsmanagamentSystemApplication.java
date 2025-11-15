e8e65fa1bc3317f5d7b5c4e4db0659160cc6c65e
Pitamber Pandey <pitamberpandey.2060@gmail.com>
2025-11-15 20:19:51 +0545

    file upload to local machine to gitup

---
+++ src/main/java/com/amsmanagament/system/AmsmanagamentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.amsmanagament.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsmanagamentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsmanagamentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amsmanagament/system/AmsmanagamentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsmanagament.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsmanagamentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
