6f5f966e8800ddfc5bc7cf320ccd40643f807f16
Pitamber Pandey <pitamberpandey.2060@gmail.com>
2026-01-05 10:04:44 +0545

    update code

---
+++ src/main/java/com/amsmanagament/system/repo/OrderItemRepo.java
@@ -0,0 +1,17 @@
+package com.amsmanagament.system.repo;
+
+import com.amsmanagament.system.model.Order;
+import com.amsmanagament.system.model.OrderItem;
+import com.amsmanagament.system.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderItemRepo extends JpaRepository<OrderItem,Long> {
+
+    Optional<OrderItem> findById(Long id);
+    List<OrderItem> findByProduct(Product product);
+    List<OrderItem> findByOrder(Order Order);
+}
